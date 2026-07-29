918969e77b42f329f68a70907abfe8c226718440
Sai-guru <saiguruprigeesh17@gmail.com>
2026-07-29 21:31:26 +0530

    location matches

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,18 +1,24 @@
 package com.example.demo.config;
 
-import com.example.demo.DemoApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
 
 //this is a config file to spring
 
 @Configuration
 @EnableWebSecurity   //bypassing basic spring security , use this below implemented..
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     @Bean
@@ -55,5 +61,16 @@ public class SecurityConfig {
     // return http.build();
 
     }
+   
+     //customizing UserDetailsService
+    //  @Bean
+    //  public UserDetailsService userDetailsService() {
+
+    //     UserDetails user1 = User
+
+    //     .
+    //     return new InMemoryUserDetailsManager() ;
+    //  }
+
 
 }
